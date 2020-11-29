9539dd417864c868157b9759ef2b470da831f500
sinthushan <sinthushan@gmail.com>
2020-11-28 21:28:46 -0500

    format JSON to display correctly

--- app.py
+++ app.py
@@ -12,21 +12,25 @@ def index():
         return render_template('index.html', task =  clean_output(response_data)[:-1] )
     return render_template('index.html', task = "")
 
-def clean_output(response_data, n = 4):
+def clean_output(response_data, n = 4, python_trace = ""):
     indents = '&nbsp' * n
     cleaned_output = ""
     if type(response_data) == dict:
         cleaned_output = cleaned_output + "{"
         for key in response_data:
-            cleaned_output = cleaned_output + "<br>" + indents + key + ": " + clean_output(response_data[key], n *2)
+            python_trace = python_trace + "key:" +  key + ";" 
+            cleaned_output = cleaned_output + "<br>" + indents + key + ": " + clean_output(response_data[key], n *2, python_trace)
+            python_trace = ";".join(python_trace[:-1].split(";").pop())
         cleaned_output = cleaned_output[:-1] + "<br>" + indents[:int(len(indents)/2)] + "},"
     elif type(response_data) == list:
         cleaned_output = cleaned_output + "["
-        for element in response_data:
-            cleaned_output = cleaned_output + "<br>" + indents  + clean_output(element, n *2)
+        for index, element in enumerate(response_data):
+            python_trace = python_trace + "index:" +  str(index) + ";"
+            cleaned_output = cleaned_output + "<br>" + indents  + clean_output(element, n *2, python_trace)
+            python_trace = ";".join(python_trace[:-1].split(";").pop())
         cleaned_output = cleaned_output[:-1] + "<br>" + indents[:int(len(indents)/2)]  + "],"
     else:
-        cleaned_output = cleaned_output + str(response_data) + ","
+        cleaned_output = cleaned_output + f'<span id="{python_trace[:-1]}">' + str(response_data) + '</span>,'
     return cleaned_output
 
 if __name__ == '__main__':
