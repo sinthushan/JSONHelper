2413f340c0b8a57c5ce740b3859efda77e76e693
sinthushan <sinthushan@gmail.com>
2020-11-28 23:00:31 -0500

    code to output python code showing how to get to data point

--- app.py
+++ app.py
@@ -4,13 +4,16 @@ import json
 app = Flask(__name__)
 
 @app.route('/', methods=['Post', 'Get'])
-def index():
+@app.route('/<json_path>', methods=['Post', 'Get'])
+def index(json_path = ""):
     if request.method == 'POST':
         API_URL = request.form['API_URL']
-        print(API_URL)
         response_data = requests.get(API_URL).json()
-        return render_template('index.html', task =  clean_output(response_data)[:-1] )
-    return render_template('index.html', task = "")
+        return render_template('index.html', json_output =  clean_output(response_data)[:-1] )
+    elif json_path != "":
+        python_code = get_python_code(json_path, "")
+        return render_template('index.html', json_output = "", python_code = python_code)
+    return render_template('index.html')
 
 def clean_output(response_data, n = 4, python_trace = ""):
     indents = '&nbsp' * n
@@ -20,18 +23,32 @@ def clean_output(response_data, n = 4, python_trace = ""):
         for key in response_data:
             python_trace = python_trace + "key:" +  key + ";" 
             cleaned_output = cleaned_output + "<br>" + indents + key + ": " + clean_output(response_data[key], n *2, python_trace)
-            python_trace = ";".join(python_trace[:-1].split(";").pop())
+            python_trace = ";".join(python_trace[:-1].split(";")[:-1]) + ";" 
+            if len(python_trace) == 1:
+                python_trace = ""
         cleaned_output = cleaned_output[:-1] + "<br>" + indents[:int(len(indents)/2)] + "},"
     elif type(response_data) == list:
         cleaned_output = cleaned_output + "["
         for index, element in enumerate(response_data):
             python_trace = python_trace + "index:" +  str(index) + ";"
             cleaned_output = cleaned_output + "<br>" + indents  + clean_output(element, n *2, python_trace)
-            python_trace = ";".join(python_trace[:-1].split(";").pop())
+            python_trace = ";".join(python_trace[:-1].split(";")[:-1]) + ";" 
+            if len(python_trace) == 1:
+                python_trace = ""
         cleaned_output = cleaned_output[:-1] + "<br>" + indents[:int(len(indents)/2)]  + "],"
     else:
-        cleaned_output = cleaned_output + f'<span id="{python_trace[:-1]}">' + str(response_data) + '</span>,'
+        cleaned_output = cleaned_output + f'<a href="/{python_trace[:-1]}">' + str(response_data) + '</a>,'
     return cleaned_output
 
+def get_python_code(json_path, API_URL):
+    JSON_path_lst = json_path.split(';')
+    python_code = f"API_URL = '{API_URL}' <br> response_data = requests.get(API_URL).json() <br> response_data"
+    for key_index in JSON_path_lst:
+        index = key_index.split(":")[1]
+        if type(index) == int:
+            python_code = python_code + f"[{index}]"
+        else:
+            python_code = python_code + f"['{index}']"
+    return python_code
 if __name__ == '__main__':
     app.run(debug=True)
